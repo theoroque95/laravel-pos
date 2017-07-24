da93dd096589dc7e3d60188cc308eac0afbc7b7a
Vincent Theo Roque <theo.roque@newmeridian.co>
2017-07-24 16:31:22 +0800

    initial commit comment

---
+++ resources/assets/js/app.js
@@ -0,0 +1,37 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+window.$ = window.jQuery = require('jquery');
+
+require('../../../bower_components/jquery-ui/jquery-ui.min');
+require('../../../bower_components/bootstrap/dist/js/bootstrap.min');
+require('../../../bower_components/jquery-slimscroll/jquery.slimscroll.min');
+require('../../../bower_components/jquery-sparkline/dist/jquery.sparkline.min');
+require('../../../bower_components/jquery-knob/dist/jquery.knob.min');
+require('./plugins/jquery-jvectormap-1.2.2.min');
+require('../../../bower_components/moment/min/moment.min');
+require('../../../bower_components/bootstrap-daterangepicker/daterangepicker');
+require('../../../bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min');
+require('../../../bower_components/fastclick/lib/fastclick');
+require('./plugins/bootstrap-slider');
+require('./plugins/bootstrap-timepicker.min');
+require('./plugins/icheck.min');
+require('./plugins/pace.min');
+require('../../../bower_components/raphael/raphael.min');
+require('../../../bower_components/morris.js/morris.min');
+
+require('./BoxRefresh');
+require('./BoxWidget');
+require('./ControlSidebar');
+require('./DirectChat');
+require('./Layout');
+require('./PushMenu');
+require('./TodoList');
+require('./Tree');
+
+require('./adminlte.min');
+require('./dashboard');
+
+$.widget.bridge('uibutton', $.ui.button);
