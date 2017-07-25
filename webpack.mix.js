83c0c1d9544559fb1769b16aa0f70903990288f0
Vincent Theo Roque <theo.roque@newmeridian.co>
2017-07-25 08:52:05 +0800

    Fixed all dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .less('resources/assets/less/AdminLTE.less', 'public/css');
+   .less('resources/assets/less/AdminLTE.less', 'public/css')
+   .sass('resources/assets/sass/app.scss', 'public/css');
